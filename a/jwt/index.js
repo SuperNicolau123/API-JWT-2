afcf169e19f38e82c1401dd02eabe0aa1063126b
leoorodr <leleu45_melo@hotmail.com>
2020-10-15 17:07:56 -0300

    Coloquei as keys no exemplo

--- a/jwt/index.js
+++ a/jwt/index.js
@@ -32,8 +32,10 @@ app.post('/login', (req, res, next) => {
     if(req.body.user === 'luiz' && req.body.pwd === '123'){
       //auth ok
       const id = 1; //esse id viria do banco de dados
-      var token = jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: 300 // expires in 5min
+      var privateKey = fs.readFileSync('./private.key', 'utf8')
+      var token = jwt.sign({ id }, privateKey, {
+        expiresIn: 300, // expires in 5min
+        algorithm: "RS256" //SHA-256hash signature
       });
       return res.json({ auth: true, token: token });
     }
@@ -49,7 +51,8 @@ function verifyJWT(req, res, next){
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     
-    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+    var publicKey = fs.readFileSync('./public.key', 'utf8')
+    jwt.verify(token, publicKey, {algorithm: ["RS256"]}, function(err, decoded) {
       if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
       
       // se tudo estiver ok, salva no request para uso posterior
